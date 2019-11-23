c717d138364bd44b2727f15d368f9d07fd9db01f
Daniel Frishberg <danielpfrishberg@gmail.com>
2019-11-23 15:32:22 -0800

    works on 16x16

--- Sudoku/src/solver/Main.java
+++ Sudoku/src/solver/Main.java
@@ -29,7 +29,7 @@ public class Main {
     }
     
     public static char[][] getBoard() {
-        Example ex = new Example(16);
+        Example ex = new Example(25);
         return ex.getGrid();
     }
     
@@ -37,7 +37,7 @@ public class Main {
         int n = board.length;
         SolverInterface s;
         if (useOptimized) {
-            s = new Refactored(16);
+            s = new Refactored(25);
         }
         else {
             s = new Solver(n);
