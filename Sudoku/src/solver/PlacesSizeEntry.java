db336f08ec90af230ccef4b44a93cd2f06f2a564
Daniel Frishberg <danielpfrishberg@gmail.com>
2019-11-24 17:57:02 -0800

    missing files added

---
+++ Sudoku/src/solver/PlacesSizeEntry.java
@@ -0,0 +1,24 @@
+package solver;
+
+public class PlacesSizeEntry extends SizeEntry {
+    public static int numCreated = 0;
+    public static final int ROW = 0;
+    public static final int COL = 1;
+    public static final int BOX = 2;
+    public int index;
+    public int num;
+    public int indexType;
+    public boolean isFilledIn;
+    public PlacesSizeEntry(int index, int num, int esize, int indexType) {
+        this.index = index;
+        this.num = num;
+        this.indexType = indexType;
+        this.size = esize;
+        this.isFilledIn = false;
+        numCreated++;
+        if (numCreated > 3*625) {
+            System.out.println("TOO MANY");
+        }
+
+    }
+}
